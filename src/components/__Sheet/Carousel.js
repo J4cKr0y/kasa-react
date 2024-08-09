e6f170f7eec0fd948a6d3b44ed67049522de0fe1
J4cKr0y <Kiky270281@gmail.com>
2024-08-10 01:07:23 +0200

    Close #7 (404 page)

--- src/components/__Sheet/Carousel.js
+++ src/components/__Sheet/Carousel.js
@@ -1,15 +1,27 @@
 /*Component displaying a carousel of images with navigation buttons. */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Carousel = ({ images, currentIndex, nextSlide, prevSlide }) => {
     const visibilityStyle = images.length > 1 ? 'visible' : 'hidden';
+    const [visibilityStyleC, setVisibilityStyleC] = useState('visible');
+
+    const updateVisibilityStyleC = () => {
+        if (window.innerWidth < 1024) {
+            setVisibilityStyleC('hidden');
+        } else {
+            setVisibilityStyleC(images.length > 1 ? 'visible' : 'hidden');
+        }
+    };
+
+    useEffect(() => {
+        updateVisibilityStyleC();});
 
     return (
         <div className="__Sheet--carousel">
             <button onClick={prevSlide} className="__Sheet--carousel-button" style={{ visibility: visibilityStyle }}>❮</button>
             <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="__Sheet--carousel-image" />
             <button onClick={nextSlide} className="__Sheet--carousel-button" style={{ visibility: visibilityStyle }}>❯</button>
-            <div className="__Sheet--carousel-counter" style={{ visibility: visibilityStyle }}>{currentIndex + 1}/{images.length}</div>
+            <div className="__Sheet--carousel-counter" style={{ visibility: visibilityStyleC }}>{currentIndex + 1}/{images.length}</div>
         </div>
     );
 }
--- src/components/__Sheet/Sheet.js
+++ src/components/__Sheet/Sheet.js
@@ -1,6 +1,6 @@
 /*Component displaying details of a specific logement. */
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import data from '../../logements.json'; 
 import StarRating from './StarRating';
 import Carousel from './Carousel'; 
@@ -9,20 +9,21 @@ import './Sheet.scss'
 
 const Sheet = () => {
     const { id } = useParams(); 
-    const [logement, setLogement] = useState(null);
+    const [logement, setLogement] = useState(undefined);
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         const foundLogement = data.find((item) => item.id === id);
-        setLogement(foundLogement);
+        if (foundLogement) { setLogement(foundLogement); } 
+        else { setLogement(null); }
     }, [id]);
 
-    if (!logement) return <div>Chargement des données...</div>;
+    if (logement === undefined) return <div>Chargement des données...</div>;
+    if (logement === null) return <Navigate to="/not-found" />;
 
     const nextSlide = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % logement.pictures.length);
     };
-
     const prevSlide = () => {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + logement.pictures.length) % logement.pictures.length);
     };
@@ -35,26 +36,21 @@ const Sheet = () => {
                 nextSlide={nextSlide} 
                 prevSlide={prevSlide} 
             />
-
             <h1>{logement.title}</h1> 
             <h2>{logement.location}</h2>
             <div className="tags">
                 {logement.tags.map((tag) => (
                 <div key={tag} className="tag-button">{tag}</div>
                 ))}
-            </div>
-            
+            </div>   
             <div className="host">
                 <StarRating rating={logement.rating} size='15' />
                 <p>{logement.host.name}</p> 
                 <img src={logement.host.picture} className="hostPicture" alt={logement.host.name}/>
-            </div>
-            
+            </div>     
             <Collapse title="Description" txt={logement.description} />
-            <Collapse title="Équipements" txt={logement.equipments} />
-            
+            <Collapse title="Équipements" txt={logement.equipments} />      
         </div>
     );
 };
-
 export default Sheet;
