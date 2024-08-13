afe27d0bb0b3494d5ab173fec32ab98d4d535dbf
J4cKr0y <Kiky270281@gmail.com>
2024-08-13 23:57:16 +0200

    About page desktop version

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -10,33 +10,24 @@ function Collapse({ title, txt }) {
     setIsOpen(!isOpen);
   };
 
-
-
   const symbolStyle = {
     transform: isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
     transition: 'transform 0.5s ease-in',
     transformOrigin: '70%',
   };
 const containerStyle = {
-    overflow: 'hidden',
-    width: '90%',
     maxHeight: isOpen ? '100%' : '0',
     transition: 'max-height 0.5s ease-out',
   };
 
   const txtStyle = {
-    fontWeight: '500',
-    fontSize: '13px',
     transform: isOpen ? 'translateY(0)' : 'translateY(-100%)',
     transition: 'transform 0.5s ease-out',
-    width: '89.3%',
-    overflow: 'hidden',
-    margin: '-1rem 10% 0 10%',
   };
 
   return (
     <div className="Collapse">
-      <button className={title} onClick={handleToggle} >
+      <button className={title}  onClick={handleToggle} >
         <span>{title}</span>
         <span className="symbol" style={symbolStyle}><img src={V} alt="Symbole interactif"/></span>
       </button> <br /> 
